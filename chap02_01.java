f4ed5e29b3aa38eed16254ef332a527b56da6e4f
dahuin <wite0927@gmail.com>
2022-03-31 18:54:59 +0900

    chap02_01(1)

---
+++ chap02_01.java
@@ -0,0 +1,9 @@
+public class chap02_01 {
+    // BooleanDefaultDemo p.48
+    static boolean flag;
+    public static void main(String[] args) {
+        System.out.println("boolean 기본 값 : " +flag);
+    }
+    
+}
+//boolean 의 기본으로 대입되는 값은 false 이다.
